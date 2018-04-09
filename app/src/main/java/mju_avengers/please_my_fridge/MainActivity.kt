ab023081b2ee84beee1f3e1451ec178318b273b4
Nam Yun Hwan <nanyounan@naver.com>
2018-04-09 17:39:06 +0900

    네비게이션 추가

---
+++ app/src/main/java/mju_avengers/please_my_fridge/MainActivity.kt
@@ -0,0 +1,12 @@
+package mju_avengers.please_my_fridge
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
