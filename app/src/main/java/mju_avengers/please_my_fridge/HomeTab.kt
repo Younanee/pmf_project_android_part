3c4d92dec383d25114e20758e7db6f4a78b92534
Nam Yun Hwan <nanyounan@naver.com>
2018-04-16 18:45:59 +0900

    RecyclerView 테스트 완료, RecyclerView 에 대한 click listener 구현

--- app/src/main/java/mju_avengers/please_my_fridge/HomeTab.kt
+++ app/src/main/java/mju_avengers/please_my_fridge/HomeTab.kt
@@ -2,16 +2,59 @@ package mju_avengers.please_my_fridge
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
+import mju_avengers.please_my_fridge.adapter.FoodRecyclerAdapter
+import mju_avengers.please_my_fridge.data.testFoodData
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.support.v4.alert
+import org.jetbrains.anko.support.v4.toast
+import org.jetbrains.anko.yesButton
 
-class HomeTab : Fragment(){
+class HomeTab : Fragment(), View.OnClickListener{
+    override fun onClick(v: View?) {
+        val idx : Int = home_tab_recycler_view.getChildAdapterPosition(v)
+        alert(""+ idx + "번 인덱스 눌림!", "음식 상세보기"){
+            yesButton { toast("Good!!!") }
+            noButton {  }
+        }.show()
+        //그냥 엑티비티로 만드는게 나을듯?!?!
+        //toast( ""+ idx + "번 인덱스 눌림!")
+    }
+
+    lateinit var foodItems : ArrayList<testFoodData>
+    lateinit var foodRecyclerAdapter: FoodRecyclerAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_home, container, false)
 
         return v
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        foodItems = ArrayList()
+        foodItems.add(testFoodData(0, "음식이름1", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름2", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름3", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름4", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름5", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름6", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름7", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름8", "돼지고기 넣고 등..."))
+        foodItems.add(testFoodData(0, "음식이름9", "돼지고기 넣고 등..."))
+
+        foodRecyclerAdapter = FoodRecyclerAdapter(context!!, foodItems)
+        foodRecyclerAdapter.setOnItemClickListener(this)
+
+        home_tab_recycler_view.layoutManager = LinearLayoutManager(context)
+        home_tab_recycler_view.adapter = foodRecyclerAdapter
+
+
     }
 
 
--- app/src/main/java/mju_avengers/please_my_fridge/adapter/FoodRecyclerAdapter.kt
+++ app/src/main/java/mju_avengers/please_my_fridge/adapter/FoodRecyclerAdapter.kt
@@ -10,8 +10,16 @@ import mju_avengers.please_my_fridge.R
 import mju_avengers.please_my_fridge.data.testFoodData
 
 class FoodRecyclerAdapter(val ctx :Context, val testFoodDatas: ArrayList<testFoodData>): RecyclerView.Adapter<FoodRecyclerAdapter.Holder>() {
+    private lateinit var onItemClick : View.OnClickListener
+
+    fun setOnItemClickListener(l : View.OnClickListener){
+        onItemClick = l
+
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(ctx).inflate(R.layout.food_list_item, parent, false)
+        val view = LayoutInflater.from(ctx).inflate(R.layout.food_item, parent, false)
+        view.setOnClickListener(onItemClick)
         return Holder(view)
     }
 
@@ -20,15 +28,18 @@ class FoodRecyclerAdapter(val ctx :Context, val testFoodDatas: ArrayList<testFoo
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(testFoodDatas[position], ctx)
+        holder.foodName.text = testFoodDatas[position].name
+        holder.recipe.text = testFoodDatas[position].recipe
+        //holder.thumbnail.setImageResource()
     }
 
 
     inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView){
-        val foodName = itemView.findViewById<TextView>(R.id.tv_food_list_item_name)
-
-        fun bind(testFoodData: testFoodData, ctx: Context){
-            foodName?.text = testFoodData.name
-        }
+        val foodName : TextView = itemView.findViewById(R.id.food_item_tv_name) as TextView
+        //val thumbnail : ImageView = itemView.findViewById(R.id.food_item_thumbnail_image) as ImageView
+        val recipe : TextView = itemView.findViewById(R.id.food_item_tv_recipe) as TextView
+//        fun bind(testFoodData: testFoodData, ctx: Context){
+//            foodName?.text = testFoodData.name
+//        }
     }
 }
