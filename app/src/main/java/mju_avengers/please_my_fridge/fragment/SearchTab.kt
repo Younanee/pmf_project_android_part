f6bdaf798fc5744680ad5ef322192fae9f39fee2
nyounh notebook <nyounh@gmail.com>
2018-05-24 09:11:26 +0900

    progressbar 추가

--- app/src/main/java/mju_avengers/please_my_fridge/fragment/SearchTab.kt
+++ app/src/main/java/mju_avengers/please_my_fridge/fragment/SearchTab.kt
@@ -1,10 +1,12 @@
 package mju_avengers.please_my_fridge.fragment
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,11 +16,16 @@ import jp.wasabeef.recyclerview.adapters.SlideInLeftAnimationAdapter
 import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 import kotlinx.android.synthetic.main.fragment_search.*
 import mju_avengers.please_my_fridge.DetailedFoodInfoActivity
+import mju_avengers.please_my_fridge.OnGetDataListener
 import mju_avengers.please_my_fridge.R
+import mju_avengers.please_my_fridge.UseFirebaseDatabase
 import mju_avengers.please_my_fridge.adapter.SearchFoodRecyclerAdapter
 import mju_avengers.please_my_fridge.data.FoodData
 import mju_avengers.please_my_fridge.data.SimpleFoodData
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.support.v4.indeterminateProgressDialog
 import org.jetbrains.anko.support.v4.toast
+import org.jetbrains.anko.uiThread
 
 
 class SearchTab : Fragment(), View.OnClickListener {
@@ -31,13 +38,11 @@ class SearchTab : Fragment(), View.OnClickListener {
         startActivity(intent)
     }
 
-    //Firebase database
-    lateinit var mFoodDatabase: DatabaseReference
-
-    //recyclerView
     lateinit var simpleFoodItems: ArrayList<SimpleFoodData>
     lateinit var searchFoodRecyclerAdapter: SearchFoodRecyclerAdapter
     lateinit var slideInfoRecyclerAdapter : SlideInLeftAnimationAdapter
+    var mProgressDialog : ProgressDialog? = null
+    private var dataCount = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_search, container, false)
@@ -46,25 +51,25 @@ class SearchTab : Fragment(), View.OnClickListener {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-//        setFoodDataAdapter()
-//        mFoodDatabase = FirebaseDatabase.getInstance().reference
-        //최상위
+        setSearchFoodRecyclerAdapter()
         search_bottom_navi.setOnNavigationItemSelectedListener {
-            setFoodDataAdapter()
-            mFoodDatabase = FirebaseDatabase.getInstance().reference
             when (it.itemId) {
                 R.id.action_grocery -> {
                     if (simpleFoodItems.isNotEmpty()) {
                         clearRecyclerView()
                     }
-                    ReadDatabaseTask().execute(arrayListOf("511","1244","124"))
+                    arrayListOf("1234", "33", "4124", "3331", "121", "7", "110", "400").forEach {
+                        readAndFetchFoodItems(it, 8)
+                    }
                     true
                 }
                 R.id.action_fridge -> {
                     if (simpleFoodItems.isNotEmpty()) {
                         clearRecyclerView()
                     }
-                    ReadDatabaseTask().execute(arrayListOf("1"))
+                    arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10").forEach {
+                        readAndFetchFoodItems(it, 10)
+                    }
                     true
                 }
                 R.id.action_info -> {
@@ -82,14 +87,61 @@ class SearchTab : Fragment(), View.OnClickListener {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        search_bottom_navi.selectedItemId = R.id.action_fridge
+    }
+
+    override fun onStop() {
+        super.onStop()
+        clearRecyclerView()
+    }
+
     private fun clearRecyclerView(){
         var size = simpleFoodItems.size
         simpleFoodItems.clear()
+        search_food_rv.adapter.notifyDataSetChanged()
         search_food_rv.adapter.notifyItemRangeRemoved(0, size)
     }
 
+    private fun readAndFetchFoodItems(childID : String, count : Int) {
 
-    private fun setFoodDataAdapter() {
+        UseFirebaseDatabase.getInstence().readFBData(childID, object : OnGetDataListener {
+            override fun onStart() {
+                if (mProgressDialog == null) {
+                    mProgressDialog = indeterminateProgressDialog("데이터 불러오기...")
+                    mProgressDialog!!.show()
+                } else {
+                    if (!mProgressDialog!!.isShowing){
+                        mProgressDialog!!.show()
+                    }
+                }
+            }
+            override fun onSuccess(data: DataSnapshot) {
+                dataCount++
+
+                var id = data!!.child("id").value.toString()
+                var url = data.child("url").child("0").value.toString()
+                var title = data!!.child("title").value.toString()
+                var percent = 100
+                var starRate = 4.5.toFloat()
+                simpleFoodItems.add(SimpleFoodData(id, url, title, percent, starRate))
+                searchFoodRecyclerAdapter.notifyItemInserted(simpleFoodItems.size)
+                searchFoodRecyclerAdapter.notifyDataSetChanged()
+
+                if (count==simpleFoodItems.size && mProgressDialog!!.isShowing) {
+                    mProgressDialog!!.dismiss()
+                }
+
+            }
+
+            override fun onFailed(databaseError: DatabaseError) {
+                Log.e("FireBase DB Error", databaseError.toString())
+            }
+        })
+    }
+    private fun setSearchFoodRecyclerAdapter() {
         simpleFoodItems = ArrayList()
         searchFoodRecyclerAdapter = SearchFoodRecyclerAdapter(context!!, simpleFoodItems)
         searchFoodRecyclerAdapter.setOnItemClickListener(this)
@@ -97,50 +149,5 @@ class SearchTab : Fragment(), View.OnClickListener {
         search_food_rv.layoutManager = LinearLayoutManager(context)
         search_food_rv.itemAnimator = SlideInLeftAnimator()
         search_food_rv.adapter = slideInfoRecyclerAdapter
-    }
-
-    private fun getFoodItem(id: String){
-        mFoodDatabase.child(id).addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError?) {
-                toast("해당 데이터가 없습니다.")
-            }
-            override fun onDataChange(data: DataSnapshot?) {
-                setFoodItem(data!!)
-                search_food_rv.adapter.notifyDataSetChanged()
-            }
-        })
-    }
-    private fun setFoodItem(data : DataSnapshot){
-        var id = data.child("id").value.toString()
-        var url = data.child("url").child("0").value.toString()
-        var title = data.child("title").value.toString()
-        var percent = 100
-        var starRate = 4.5.toFloat()
-        simpleFoodItems.add(SimpleFoodData(id, url, title, percent, starRate))
-    }
-    private inner class ReadDatabaseTask : AsyncTask<ArrayList<String>, String, Boolean>() {
-        override fun onPreExecute() {
-            super.onPreExecute()
-            //foodItems.clear()
-            search_progressbar.visibility = View.VISIBLE
-        }
-
-        override fun doInBackground(vararg params: ArrayList<String>): Boolean {
-            val temp : ArrayList<String> = params[0]
-            for (id_str in temp) {
-                getFoodItem(id_str)
-            }
-            return true
-        }
-
-        override fun onPostExecute(result: Boolean?) {
-            super.onPostExecute(result)
-            if(result!!){
-                search_food_rv.adapter.notifyDataSetChanged()
-                search_progressbar.visibility = View.GONE
-            } else {
-                toast("아직")
-            }
-        }
     }
 }
